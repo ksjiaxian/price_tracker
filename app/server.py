eab67177fc87deb0b3a6b271c603d243879896cc
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-04-09 11:57:28 -0400

    Add flask app

---
+++ app/server.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def splash():
+	return render_template("splash.html")
+
+if __name__ == "__main__":
+	app.run(port=5000, debug=True)
