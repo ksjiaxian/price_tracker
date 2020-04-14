649cf961aeac8d7d1295d601283b9690996e2c2b
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-04-14 19:43:43 -0400

    Added dummy info

--- app/server.py
+++ app/server.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def splash():
 	return render_template("splash.html")
 
+@app.route("/dashboard")
+def dashboard():
+	return render_template("dashboard.html")
+
+@app.route("/timeline")
+def timeline():
+	return render_template("timeline.html")
+
+@app.route("/cart")
+def cart():
+	return render_template("cart.html")
+
 if __name__ == "__main__":
-	app.run(port=5000, debug=True)
+	app.run(port=8000, debug=True)
