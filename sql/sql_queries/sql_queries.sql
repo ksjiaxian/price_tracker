2630b55f11914f4b32f0de2b82ab6705061ed473
Varun Ramakrishnan <vramakrishnan1999@gmail.com>
2020-04-25 14:09:59 -0700

    all sql queries written, now integrating into explore

--- sql/sql_queries/sql_queries.sql
+++ sql/sql_queries/sql_queries.sql
@@ -37,51 +37,101 @@ ON s.dateID = h.dateID
 WHERE s.dateID >= 20200211
 AND s.dateID <= 20200320;
 
--- what's the average price of a stock adjusted for inflation (here we limit to the dates to when apple was a company)
-SELECT AVG(adjusted_apple)
-FROM (SELECT (AAPL/AMOUNT) as adjusted_apple
-FROM (SELECT dateID, AAPL
-FROM Stocks s
-WHERE dateID >= 19801212) a 
-JOIN Dates d ON a.dateID = d.dateID
-JOIN Inflation i ON d.Year = i.Year);
+--1. What does this company do?--
+SELECT d.descr
+FROM Descriptions d 
+WHERE d.itemID = AAPL
 
-
-
-
---in the last x days, have prices in my bundle of stocks been trending down or up?--
-
-SELECT (t.today_weighted_price - x.x_days_ago_weighted_price) price_delta
+--2. How much did this stock drop (percentage) on the worst day of the market (3-16-2020)?--
+SELECT (t.today_price - x.yesterday_price) price_delta
 FROM
-(SELECT (s.AAPL + s.AMZN)/2 as today_weighted_price
+(SELECT s.AAPL as today_price
 FROM Stocks s 
-WHERE s.dateID = 20200323) t,
-(SELECT (s.AAPL + s.AMZN)/2 as x_days_ago_weighted_price
+WHERE s.dateID = 20200316) t,
+(SELECT s.AAPL as yesterday_price
 FROM Stocks s 
 WHERE s.dateID = 20200313) x ;
 
---How volatile is the price of Super Mario 64 over the last x months? (here x=8)--
-SELECT (t.today_mario - p.past_mario) price_delta
+--3. How much did the stock change when markets reopened after 9/11 (20010917)?--
+SELECT (t.today_price - x.yesterday_price) price_delta
 FROM
-(SELECT c.MARIO as today_mario
+(SELECT s.AAPL as today_price
+FROM Stocks s 
+WHERE s.dateID = 20010917) t,
+(SELECT s.AAPL as yesterday_price
+FROM Stocks s 
+WHERE s.dateID = 20010910) x ;
+
+--4. what's the average price of a stock adjusted for inflation? (since 2000)--
+SELECT AVG(adjusted_value)
+FROM (SELECT (AAPL/AMOUNT) as adjusted_value
+FROM (SELECT dateID, AAPL
+FROM Stocks s
+WHERE dateID >= 20000103) a 
+JOIN Dates d ON a.dateID = d.dateID
+JOIN Inflation i ON d.Year = i.Year);
+
+--5. in the last x days, has this item been trending down or up?--
+
+SELECT (t.today_price - x.yesterday_price) price_delta
+FROM
+(SELECT s.AAPL as today_price
+FROM Stocks s 
+WHERE s.dateID = 20200323) t,
+(SELECT s.AAPL as yesterday_price
+FROM Stocks s 
+WHERE s.dateID = 20200313) x ;
+
+--6. How volatile is the price of Super Mario 64 over the last x months? (here x=8)--
+SELECT (t.today_value - p.past_value) price_delta
+FROM
+(SELECT c.MARIO as today_value
 FROM Commodities c 
 WHERE c.dateID = 20200102) t,
-(SELECT c.MARIO as past_mario
+(SELECT c.MARIO as past_value
 FROM Commodities c
 WHERE c.dateID = 20190502) p ;
 
---Return the higher inflation-adjusted value of the Valentino Beanie Baby or Sears? (in 2006)--
+--7. Return the higher inflation-adjusted value of _______ or Sears? (in 2006)--
 WITH Sears AS (SELECT (SHLDQ/AMOUNT) as adjusted_sears
 FROM (SELECT dateID, SHLDQ
 FROM Stocks s
 WHERE dateID = 20060323) a 
 JOIN Dates d ON a.dateID = d.dateID
 JOIN Inflation i ON d.Year = i.Year),
-Valentino AS (SELECT (Valentino/AMOUNT) as adjusted_valentino
+Valentino AS (SELECT (Valentino/AMOUNT) as adjusted_product
 FROM (SELECT dateID, Valentino
 FROM Commodities c
 WHERE dateID = 20060323) a 
 JOIN Dates d ON a.dateID = d.dateID
 JOIN Inflation i ON d.Year = i.Year)
-SELECT GREATEST(v.adjusted_valentino, s.adjusted_sears) as higher_value
+SELECT GREATEST(v.adjusted_product, s.adjusted_sears) as higher_value
 FROM Sears s, Valentino v;
+
+--8. Has this item's percentage return beat the market from 2017 onwards? --
+SELECT ((tm.today_mkt_price - xm.yesterday_mkt_price)/xm.yesterday_mkt_price*100) AS market_return, 
+((t.today_item_price - x.yesterday_item_price)/x.yesterday_item_price*100) AS item_return
+FROM
+(SELECT s.AAPL as today_item_price
+FROM Stocks s 
+WHERE s.dateID = 20200323) t,
+(SELECT s.AAPL as yesterday_item_price
+FROM Stocks s 
+WHERE s.dateID = 20170103) x,
+(SELECT i.SP as today_mkt_price
+FROM Indexes i 
+WHERE i.dateID = 20200323) tm,
+(SELECT i.SP as yesterday_mkt_price
+FROM Indexes i 
+WHERE i.dateID = 20170103) xm;
+
+--9. Does this item beat the risk free rate of the market (10 yr T-bills) from 2017 onwards? --
+-- https://dqydj.com/treasury-return-calculator/ --
+SELECT ((t.today_item_price - x.yesterday_item_price)/x.yesterday_item_price*100) AS item_return, 17.067 AS tbill_return
+FROM
+(SELECT s.AAPL as today_item_price
+FROM Stocks s 
+WHERE s.dateID = 20200323) t,
+(SELECT s.AAPL as yesterday_item_price
+FROM Stocks s 
+WHERE s.dateID = 20170103) x
