64ed0ef0a4d5c43307225741ced94dff831eddc0
Kenneth Shinn <kshinn@sas.upenn.edu>
2020-05-03 17:17:41 -0400

    small change to sql queries

--- sql/sql_queries/sql_queries.sql
+++ sql/sql_queries/sql_queries.sql
@@ -1,49 +1,69 @@
--- Price Tracker! --
---Queries to be used for the project--
+-- 1. Obtain all price data over time - 2.15 sec
+SELECT dateID, DOW 
+FROM Indexes s
+WHERE dateID >= 19700101;
 
---what was the price of a good on a day?--
-Select history as new_today
-From History h
-Where h.dateid = 19970522;
-
-SELECT MIN( AMZN ) FROM STOCKS s WHERE s.AMZN > 0 AND dateID >= 20200314;
-SELECT AMZN FROM Stocks s WHERE s.dateID = 20011105;
-
-SELECT s.chk
+-- 2. What is the current price of the item? - 0.571 sec
+SELECT AAPL 
 FROM Stocks s 
-WHERE s.dateID = 20100326;
+WHERE s.dateID = 20200409;
 
-/* we can replace Oil with another good and the data id with another date. */
+-- 3. What was the price of an item on a 
+--    specific date (purchasing stock)? - 0.501 sec
+SELECT AMZN 
+FROM Stocks s 
+WHERE s.dateID = 20180514;
 
--- get the price adjusted for inflation (i.e. how much would this cost in 1980?):
-SELECT (AAPL / adjustment_rate.AMOUNT) as adjusted_AAPL
-FROM Stocks s, (
-SELECT Amount 
-FROM Inflation i
-WHERE i.Year = 1980) adjustment_rate
-WHERE s.dateID = 20200324;
+-- 4. What is the historical max price? - 0.511 sec
+SELECT MAX(AAPL) AS max_price
+FROM Stocks s 
+WHERE s.AAPL > 0 AND dateID >= 19700101;
 
---get all of the prices of specific goods from the last x days--
---suppose that the date x days ago has already been calculated, call it --
-SELECT s.AAPL as apple_stock, i.nasdaq as nasdaq
-FROM Indexes i JOIN Stocks s
-ON i.dateID = s.dateID
-WHERE i.dateID >= 20200311;
+-- 5. What is the date of the historical max price? - 0.505 sec
+SELECT dateID AS max_date
+FROM Stocks s 
+WHERE s.AAPL = (
+    SELECT MAX(AAPL) 
+    FROM Stocks s 
+    WHERE s.AAPL > 0 AND dateID >= 19700101)
+    AND dateID >= 19700101;
 
--- given a series of days, see how each day's current events affect the stock prices
-SELECT s.AAPL as Apple, s.AMZN as Amazon, s.JCP as JCPenney, s.GE as GE, s.BB as BlackBerry, h.history
-FROM Stocks s JOIN History h
-ON s.dateID = h.dateID
-WHERE s.dateID >= 20200211
-AND s.dateID <= 20200320;
+-- 6. What historical event occurred on the max price date?
+-- (max_date is the output of the above query) - 0.511 sec
+SELECT history AS new_today 
+FROM History h 
+WHERE h.dateid = 'max_date';
 
---1. What does this company do?--
+-- 7. What is the historical min price? - 0.49 sec
+SELECT MIN(AAPL) AS min_price
+FROM Stocks s 
+WHERE s.AAPL > 0 AND dateID >= 19700101;
+
+-- 8. What is the date of the historical min price? - 0.512 sec
+SELECT dateID AS min_date
+FROM Stocks s 
+WHERE s.AAPL = (
+    SELECT MIN(AAPL) 
+    FROM Stocks s 
+    WHERE s.AAPL > 0 AND dateID >= 19700101)
+    AND dateID >= 19700101;
+
+-- 9. What historical event occurred on the min price date?
+-- (max_date is the output of the above query) - 0.513 sec
+SELECT history as new_today 
+FROM History h 
+WHERE h.dateid = 'max_date';
+
+-- QUERIES FOR EXPLORE ITEM PAGE --
+--1. What does this company do?-- 0.518 sec
 SELECT d.desr
 FROM Descriptions d 
 WHERE d.itemID = AAPL
 
---2. How much did this stock drop (percentage) on the worst day of the market (3-16-2020)?--
-SELECT ((t.today_price - x.yesterday_price)/x.yesterday_price * 100) price_delta
+--2. How much did this stock drop (percentage) 
+--   on the worst day of the market? - 0.955 sec
+SELECT ((t.today_price - x.yesterday_price)/
+    x.yesterday_price * 100) price_delta
 FROM
 (SELECT s.AAPL as today_price
 FROM Stocks s 
@@ -52,8 +72,10 @@ WHERE s.dateID = 20200316) t,
 FROM Stocks s 
 WHERE s.dateID = 20200313) x ;
 
---3. How much did the stock change when markets reopened after 9/11 (20010917)?--
-SELECT ((t.today_price - x.yesterday_price)/(x.yesterday_price+0.0001)) price_delta
+--3. How much did the stock change when markets 
+--   reopened after 9/11? - 0.494 sec
+SELECT ((t.today_price - x.yesterday_price)/
+    (x.yesterday_price+0.0001)) price_delta
 FROM
 (SELECT s.AAPL as today_price
 FROM Stocks s 
@@ -62,7 +84,8 @@ WHERE s.dateID = 20010917) t,
 FROM Stocks s 
 WHERE s.dateID = 20010910) x ;
 
---4. what's the average price of a stock adjusted for inflation? (since 2000)--
+--4. What's the average price of a stock adjusted
+--   for inflation? (since 2000) - 0.511 sec
 SELECT AVG(adjusted_value)
 FROM (SELECT (AAPL/AMOUNT) as adjusted_value
 FROM (SELECT dateID, AAPL
@@ -71,7 +94,8 @@ WHERE dateID >= 20000103) a
 JOIN Dates d ON a.dateID = d.dateID
 JOIN Inflation i ON d.Year = i.Year);
 
---5. in the last x days, has this item been trending down or up?--
+--5. In the last 10 days, has this item been 
+--   trending down or up? - 0.491 sec
 
 SELECT (t.today_price - x.yesterday_price) price_delta
 FROM
@@ -82,7 +106,8 @@ WHERE s.dateID = 20200323) t,
 FROM Stocks s 
 WHERE s.dateID = 20200313) x ;
 
---6. How volatile is the price of Super Mario 64 over the last x months? (here x=8)--
+--6. How volatile is the price of Super Mario 64 over 
+--   the last 8 months? - 0.491 sec
 SELECT (t.today_value - p.past_value) price_delta
 FROM
 (SELECT c.MARIO as today_value
@@ -92,7 +117,8 @@ WHERE c.dateID = 20200102) t,
 FROM Commodities c
 WHERE c.dateID = 20190502) p ;
 
---7. Return the difference between the inflation-adjusted values of ___ and Sears in 2006--
+--7. Return the difference between the inflation-adjusted 
+--   values of Valentino and Sears in 2006. - .502 sec
 WITH Sears AS (SELECT (SHLDQ/AMOUNT) as adjusted_sears
 FROM (SELECT dateID, SHLDQ
 FROM Stocks s
@@ -108,9 +134,12 @@ JOIN Inflation i ON d.Year = i.Year)
 SELECT v.adjusted_product - s.adjusted_sears as difference
 FROM Sears s, Valentino v;
 
---8. Has this item's percentage return beat the market from 2017 onwards? --
-SELECT ((tm.today_mkt_price - xm.yesterday_mkt_price)/xm.yesterday_mkt_price*100) AS market_return, 
-((t.today_item_price - x.yesterday_item_price)/x.yesterday_item_price*100) AS item_return
+--8. Has this item's percentage return beat 
+--   the market from 2017 onwards? - 0.510 sec
+SELECT ((tm.today_mkt_price - xm.yesterday_mkt_price)/
+    xm.yesterday_mkt_price*100) AS market_return, 
+    ((t.today_item_price - x.yesterday_item_price)/
+    x.yesterday_item_price*100) AS item_return
 FROM
 (SELECT s.AAPL as today_item_price
 FROM Stocks s 
@@ -125,9 +154,11 @@ WHERE i.dateID = 20200323) tm,
 FROM Indexes i 
 WHERE i.dateID = 20170103) xm;
 
---9. Does this item beat the risk free rate of the market (10 yr T-bills) from 2017 onwards? --
--- https://dqydj.com/treasury-return-calculator/ --
-SELECT ((t.today_item_price - x.yesterday_item_price)/x.yesterday_item_price*100) AS item_return, 17.067 AS tbill_return
+--9. Does this item beat the risk free rate of the 
+--   market (10 yr T-bills) from 2017 onwards? - 0.508 sec
+-- Source: https://dqydj.com/treasury-return-calculator/
+SELECT ((t.today_item_price - x.yesterday_item_price)/
+    x.yesterday_item_price*100) AS item_return, 17.067 AS tbill_return
 FROM
 (SELECT s.AAPL as today_item_price
 FROM Stocks s 
@@ -135,3 +166,19 @@ WHERE s.dateID = 20200323) t,
 (SELECT s.AAPL as yesterday_item_price
 FROM Stocks s 
 WHERE s.dateID = 20170103) x
+
+--10. How many times was the share price of this item 
+--    the same price as another item (limit 2000) while the other item was in business?
+SELECT count(comparator) as num_of_same_price
+FROM(SELECT AAPL AS comparator FROM Stocks s JOIN Commodities c ON s.dateID = c.dateID 
+    JOIN Indexes i ON s.dateID = i.dateID WHERE AAPL <> 0 AND AMZN <> 0) t
+WHERE Exists (SELECT AMZN as comparator FROM Stocks s JOIN Commodities c ON s.dateID = c.dateID 
+    JOIN Indexes i ON s.dateID = i.dateID WHERE AMZN <> 0) ;
+    
+-- after (the good one)    
+SELECT count(comparator)
+FROM
+(SELECT AAPL as comparator FROM Stocks s WHERE AAPL <> 0 AND AMZN <> 0) t
+WHERE Exists (SELECT AMZN as comparator FROM Stocks s WHERE AMZN <> 0) ;    
+    
+    
